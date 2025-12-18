a1428b6b3c86d808745da0c62775d1fe076abbf9
HariKrishnan-sparient <HariKrishnan@sparient.com>
2025-12-18 13:06:38 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using GeneratePDF;
+using GeneratePDF.Models;
+using System.Text.Json;
+
+
+var projectRoot = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, @"..\..\.."));
+var requestJson = File.ReadAllText(Path.Combine(projectRoot, "RequestData", "PropertyRequest.json"));
+var requestData = JsonSerializer.Deserialize<PropertyReportDto>(requestJson);
+var pdfFolderPath = Path.Combine(projectRoot, "PDF");
+string templatePathScriban = Path.Combine(projectRoot, "Template", "PropertyReport_Tem.html");
+var pdfFilePath = Path.Combine(pdfFolderPath, $"PropertyReport_{DateTime.Now:yyyyMMddHHmmssfff}.pdf");
+
+string renderedHtml = ScribanHelper.RenderTemplate(requestData, templatePathScriban);
+
+var renderer = new IronPdf.ChromePdfRenderer();
+
+var pdf = renderer.RenderHtmlAsPdf(renderedHtml);
+pdf.SaveAs(pdfFilePath);
+
+Console.WriteLine("PDF generated successfully."); 
---
+++ ScribanHelper.cs
@@ -0,0 +1,14 @@
+﻿using Scriban;
+
+namespace GeneratePDF
+{
+    public static class ScribanHelper
+    {
+        public static string RenderTemplate(object model, string templatePath)
+        {
+            var templateText = File.ReadAllText(templatePath);
+            var template = Template.Parse(templateText);
+            return template.Render(model, member => member.Name);
+        }
+    }
+}
